f69fe7cffdc5d193e0e8d1f4f7912606c3bfc7f5
Tomin Paul <tominpaul123@gmail.com>
2022-02-23 01:25:52 -0500

    Added ErrorModal for Empty Password in SignIn Form

--- src/Components/SignIn/UserSignIn.js
+++ src/Components/SignIn/UserSignIn.js
@@ -3,12 +3,14 @@ import axios from "axios";
 import styles from "./UserSignIn.module.css";
 import Card from "../UI/Card/Card";
 import Button from "../UI/Button/Button";
+import ErrorModal from "../UI/ErrorModal/ErrorModal";
 import "@fontsource/roboto";
 
 const UserSignIn = (props) => {
   const [enteredEmail, setEnteredEmail] = useState("");
   const [enteredPassword, setenteredPassword] = useState("");
   const [isValid, setIsValid] = useState(true);
+  const [error, setError] = useState();
 
   const emailInputHandler = (event) => {
     if (event.target.value.trim().length > 0) {
@@ -28,67 +30,86 @@ const UserSignIn = (props) => {
   const userSignInHandler = (event) => {
     event.preventDefault(); //prevent sending default request while clicking Sign in button
 
-    if (
-      enteredEmail.trim().length === 0 ||
-      enteredPassword.trim().length === 0
-    ) {
+    if (enteredEmail.trim().length === 0) {
       setIsValid(false);
       return;
     }
 
-    const userObject = {
-      email: enteredEmail,
-      password: enteredPassword,
-    };
-
-    axios
-      .post("localhost:8000/api/login", userObject)
-      .then((res) => {
-        console.log(res.data);
-      })
-      .catch((error) => {
-        console.log(error);
+    if (enteredPassword.trim().length === 0) {
+      setError({
+        title: "Empty Password Field",
+        message: "Please enter your password!",
       });
+    } else {
+      const userObject = {
+        email: enteredEmail,
+        password: enteredPassword,
+      };
 
-    setEnteredEmail("");
-    setenteredPassword("");
+      axios
+        .post("localhost:8000/api/login", userObject)
+        .then((res) => {
+          console.log(res.data);
+        })
+        .catch((error) => {
+          console.log(error);
+        });
+
+      setEnteredEmail("");
+      setenteredPassword("");
+    }
+  };
+
+  const errorHandler = () => {
+    setError(null);
   };
 
   return (
-    <Card className={`${styles["form-control"]} ${!isValid && styles.invalid}`}>
-      <form onSubmit={userSignInHandler}>
-        <h1>Sign in</h1>
-        <label htmlFor="email">Email</label>
-        <input
-          id="email"
-          type="email"
-          value={enteredEmail}
-          onChange={emailInputHandler}
+    <div>
+      {error && (
+        <ErrorModal
+          title={error.title}
+          message={error.message}
+          onConfirm={errorHandler}
         />
-        <label htmlFor="password">Password</label>
-        <input
-          id="password"
-          type="password"
-          value={enteredPassword}
-          onChange={passwordInputHandler}
-          autoComplete="off"
-        />
-        <span>
-          <input id="remember-me" type="checkbox" />
-          Remember me?
-        </span>
-        <Button type="submit">Sign in</Button>
-        <p>
-          <a href="url">Forgot your password?</a>
-        </p>
-        <p>
-          Don't have an account?<a href="url">Sign up</a>
-        </p>
-        <p>
-          <a href="url">Resend email confirmation</a>
-        </p>
-      </form>
-    </Card>
+      )}
+      <Card
+        className={`${styles["form-control"]} ${!isValid && styles.invalid}`}
+      >
+        <form onSubmit={userSignInHandler}>
+          <h1>Sign in</h1>
+          <label htmlFor="email">Email</label>
+          <input
+            id="email"
+            type="email"
+            value={enteredEmail}
+            onChange={emailInputHandler}
+          />
+          <label htmlFor="password">Password</label>
+          <input
+            id="password"
+            type="password"
+            value={enteredPassword}
+            onChange={passwordInputHandler}
+            autoComplete="off"
+          />
+          <span>
+            <input id="remember-me" type="checkbox" />
+            Remember me?
+          </span>
+          <Button type="submit">Sign in</Button>
+          <p>
+            <a href="url">Forgot your password?</a>
+          </p>
+          <p>
+            Don't have an account?<a href="url">Sign up</a>
+          </p>
+          <p>
+            <a href="url">Resend email confirmation</a>
+          </p>
+        </form>
+      </Card>
+    </div>
   );
 };
 
